f5a59eded55a1b208f6f9f5e32c72d8c2142e984
github username <bballwiz4@gmail.com>
2020-08-24 09:28:49 -0700

    The mortality rate is now ddisplayed.

--- src/components/USTotalCard.js
+++ src/components/USTotalCard.js
@@ -1,25 +1,26 @@
 import React from 'react';
 
 const USTotalCard = (props) => {
+    
     return (
         <div className="row">
             <div className="six columns">
                 <div className="row">
                     <h5>Total Positive</h5>
                     <div className="twelve columns total-numbers">
-                        <p>{props.results.positive}</p>
+                        <p>{props.results.countryCurrentResults.positive}</p>
                     </div>
                 </div>
                 <div className="row">
                     <h5>Total Test Taken</h5>
                     <div className="twelve columns total-numbers">
-                        <p>{props.results.totalTestResults}</p>
+                        <p>{props.results.countryCurrentResults.totalTestResults}</p>
                     </div>
                 </div>
                 <div className="row">
                     <h5>Positive Test Increase</h5>
                     <div className="twelve columns total-numbers">
-                        <p>{props.results.positiveIncrease}</p>
+                        <p>{props.results.countryCurrentResults.positiveIncrease}</p>
                     </div>
                 </div>
             </div>
@@ -27,19 +28,32 @@ const USTotalCard = (props) => {
                 <div className="row">
                     <h5>Total Fatalities</h5>
                     <div className="twelve columns total-numbers">
-                        <p>{props.results.death}</p>
+                        <p>{props.results.countryCurrentResults.death}</p>
                     </div>
                 </div>
                 <div className="row">
                     <h5>Fatality Increase</h5>
                     <div className="twelve columns total-numbers">
-                        <p>{props.results.deathIncrease}</p>
+                        <p>{props.results.countryCurrentResults.deathIncrease}</p>
                     </div>
                 </div>
                 <div className="row">
                     <h5>Total Recovered</h5>
                     <div className="twelve columns total-numbers">
-                        <p>{props.results.recovered}</p>
+                        <p>{props.results.countryCurrentResults.recovered}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="row">
+                    <div className="twelve columns">
+                        <h5>Mortality Rate</h5>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="twelve columns">
+                        <p>{props.results.mortalityRate}%</p>
+                        <p>The mortality rate is calculated by taking the total deaths divided by the total positive cases.</p>
                     </div>
                 </div>
             </div>
--- src/components/USTotalCardContainer.js
+++ src/components/USTotalCardContainer.js
@@ -4,13 +4,25 @@ import USTotalCard from './USTotalCard';
 export default class USTotalCardContainer extends Component {
 
     state = {
-        countryCurrentResults: {}
+        countryCurrentResults: {},
+        mortalityRate: 0
     }
 
     componentDidMount() {
         fetch('https://api.covidtracking.com/v1/us/current.json')
             .then(res => res.json())
-            .then(data => this.setState({countryCurrentResults: data[0]}))
+            .then(data => {
+                    this.setState({
+                        countryCurrentResults: data[0],
+                        mortalityRate: this.calculateMortalityRate(data[0].positive, data[0].death)
+                    })
+
+                })
+    }
+
+    calculateMortalityRate = (positive, deaths) => {
+        let rate = (deaths / positive) * 100;
+        return rate.toFixed(2)
     }
 
     render() {
@@ -19,7 +31,7 @@ export default class USTotalCardContainer extends Component {
                 <h2>
                     US Total Numbers 
                 </h2>
-                <USTotalCard results={this.state.countryCurrentResults}/>
+                <USTotalCard results={this.state}/>
             </div>
         )
     }
